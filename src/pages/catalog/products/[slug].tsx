a3d0486bae5cbba5d550a147ea03810efe7860b3
heydrigh <heydrigh@gmail.com>
2020-11-12 18:53:09 -0300

    initial commit

---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,51 @@
+import { useRouter } from 'next/router';
+import PrismicDOM from 'prismic-dom';
+import { Document } from 'prismic-javascript/types/documents';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { client } from '@/lib/prismic';
+
+interface ProductProps {
+  product: Document;
+}
+
+export default function Product({ product }: ProductProps) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <p>Carregando...</p>
+  }
+  
+  return (
+    <div>
+      <h1>
+        {PrismicDOM.RichText.asText(product.data.title)}
+      </h1>
+
+      <img src={product.data.thumbnail.url} width="400" alt=""/>
+      
+      <div dangerouslySetInnerHTML={{ __html: PrismicDOM.RichText.asHtml(product.data.description)}}></div>
+
+      <p>Price ${product.data.price}</p>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  }
+};
+
+export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
+  const { slug } = context.params;
+
+  const product = await client().getByUID('product', String(slug), {});
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 5,
+  }
+}
